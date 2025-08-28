04fc8a2b3a7a362c6894043976113c086317ebc7
g1tmaster <153743369+g1tmaster@users.noreply.github.com>
2025-08-28 23:54:43 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,9 @@ import  {Hono}  from "hono";
 import {cors} from 'hono/cors';
 import posts from './posts.js';
 import users from "./users.js";
-import {handle} from '@vercel/hono';
+import pkg from '@vercel/hono';
+const { handle } = pkg;
+
 
 
 const app = new Hono();
@@ -24,5 +26,6 @@ const port = Number(process.env.PORT || 8787);
 //for local deployment run this below codes -->
 // serve({fetch: app.fetch, port});
 // console.log(`Server running at http://localhost:${port}`);
+
 
 export default handle(app);
